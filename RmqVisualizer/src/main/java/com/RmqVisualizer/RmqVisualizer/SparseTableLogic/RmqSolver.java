9e00f821de8fbc516b8df15867d758411d32fd1f
CojocariuAlexandru <53466892+CojocariuAlexandru@users.noreply.github.com>
2020-05-31 10:09:50 +0300

    completed algorithm

--- RmqVisualizer/src/main/java/com/RmqVisualizer/RmqVisualizer/SparseTableLogic/RmqSolver.java
+++ RmqVisualizer/src/main/java/com/RmqVisualizer/RmqVisualizer/SparseTableLogic/RmqSolver.java
@@ -31,7 +31,7 @@ public class RmqSolver {
      * @param numbers
      * @return
      */
-    private List<Integer> getPrecalculationNumbers(List<Integer> numbers){
+    public List<Integer> getPrecalculationNumbers(List<Integer> numbers){
         // Algorithm explanation: https://cp-algorithms.com/data_structures/sparse-table.html
         int i, j;
 
@@ -44,10 +44,48 @@ public class RmqSolver {
         // Recursive case
         for(i=2; (1 << (i-1)) <= numbers.size(); i++){
             for(j=1; j + powersOfTwo.get(i-2) <= numbers.size(); j++){
-
+                precalculatedNumbers.add(Math.min(precalculatedNumbers.get(translateMatrixIntoArrayCoordinates(i-1, j, numbers.size()))
+                                                , precalculatedNumbers.get(translateMatrixIntoArrayCoordinates(i-1, j+powersOfTwo.get(i-2), numbers.size()))
+                                        )
+                );
             }
         }
         return precalculatedNumbers;
     }
 
+    public Integer getMinimumFromRange(List<Integer> numbers, List<Integer> precalculatedNumbers, Integer leftIndex, Integer rightIndex){
+        Integer segmentLength;
+        Integer powerOfTwoUsed;
+        segmentLength = rightIndex - leftIndex + 1;
+        powerOfTwoUsed = smallestPowerOfTwoSmallerThan(segmentLength);
+        return Math.min(precalculatedNumbers.get(translateMatrixIntoArrayCoordinates(powerOfTwoUsed+1, leftIndex, numbers.size())),
+                        precalculatedNumbers.get(translateMatrixIntoArrayCoordinates(powerOfTwoUsed+1, leftIndex+segmentLength-powersOfTwo.get(powerOfTwoUsed), numbers.size()))
+        );
+    }
+
+    private Integer translateMatrixIntoArrayCoordinates(Integer line, Integer column, Integer numbersSize){
+        line = line - 1;
+        return line*numbersSize - (powersOfTwo.get(line) - 1 - line) + column;
+    }
+
+    private Integer smallestPowerOfTwoSmallerThan(Integer maximum){
+        Integer leftIndex = -1;
+        Integer rightIndex = 35;
+        Integer middleIndex = 0;
+        while(rightIndex - leftIndex > 1){
+            middleIndex = (leftIndex + rightIndex) / 2;
+            if(powersOfTwo.get(middleIndex) < maximum){
+                leftIndex = middleIndex;
+            }
+            else{
+                rightIndex = middleIndex;
+            }
+        }
+        if(maximum == powersOfTwo.get(rightIndex)){
+            return rightIndex;
+        }
+        else{
+            return rightIndex - 1;
+        }
+    }
 }
